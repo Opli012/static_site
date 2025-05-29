bbd2a49c2d0a63b712a503b07bdb37d958456c42
József <opli012gm@gmail.com>
2025-05-29 15:18:50 +0200

    Extract Images and Links

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -51,13 +51,13 @@ class TestTextNodeToHTMLNode(unittest.TestCase):
         self.assertEqual(html_node.value, "This is a text node")
 
     def test_image(self):
-        node = TextNode("This is an image", TextType.IMAGE, "https://www.boot.dev")
+        node = TextNode("This is an image", TextType.IMAGE, "https://www.youtube.com")
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "img")
         self.assertEqual(html_node.value, "")
         self.assertEqual(
             html_node.props,
-            {"src": "https://www.boot.dev", "alt": "This is an image"},
+            {"src": "https://www.youtube.com", "alt": "This is an image"},
         )
 
     def test_bold(self):
